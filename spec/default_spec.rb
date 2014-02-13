d39a9553f10f11f792d9a3003dede4ae3436ac77
Sean OMeara <someara@opscode.com>
2014-02-13 16:44:53 -0500

    Updating test harness

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -2,15 +2,10 @@ require 'spec_helper'
 
 describe 'yum-erlang_solutions::default' do
   context 'yum-erlang_solutions::default uses default attributes' do
-    let(:chef_run) { ChefSpec::Runner.new(:step_into => ['yum_repository']).converge(described_recipe) }
+    let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
 
     it 'creates yum_repository[erlang_solutions]' do
       expect(chef_run).to create_yum_repository('erlang_solutions')
     end
-
-    it 'steps into yum_repository and creates template[/etc/yum.repos.d/erlang_solutions.repo]' do
-      expect(chef_run).to render_file('/etc/yum.repos.d/erlang_solutions.repo')
-    end
-
   end
 end
