21fcb2931ca04dd015907756bf89b4e40b981823
Tim Smith <tsmith@chef.io>
2015-09-07 13:25:34 -0700

    Update maintainer

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'yum-erlang_solutions'
-maintainer       'Chef'
-maintainer_email 'Sean OMeara <someara@chef.io>'
+maintainer       'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
 description      'Installs/Configures yum-erlang_solutions'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
