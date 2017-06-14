907a3afefbc0c915dbad737148e8a9d4e4a4c5b1
Tim Smith <tsmith@chef.io>
2017-06-14 16:48:57 -0700

    Avoid chefspec deprecations
    Also test centos 6 converge
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,19 +1,11 @@
 require 'spec_helper'
 
 describe 'yum-erlang_solutions::default' do
-  context 'yum-erlang_solutions::default uses default attributes' do
-    let(:chef_run) { ChefSpec::ServerRunner.new.converge(described_recipe) }
-
-    it 'creates yum_repository[erlang_solutions]' do
-      expect(chef_run).to create_yum_repository('erlang_solutions')
-    end
-  end
-
-  context 'centos-7.0' do
+  context 'centos-7' do
     let(:chef_run) do
-      ChefSpec::ServerRunner.new(
+      ChefSpec::SoloRunner.new(
         platform: 'centos',
-        version: '7.0'
+        version: '7.3.1611'
       ).converge(described_recipe)
     end
 
@@ -21,6 +13,22 @@ describe 'yum-erlang_solutions::default' do
       expect(chef_run).to create_yum_repository('erlang_solutions').with(
         repositoryid: 'erlang_solutions',
         baseurl: 'http://packages.erlang-solutions.com/rpm/centos/7/$basearch'
+      )
+    end
+  end
+
+  context 'centos-6' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new(
+        platform: 'centos',
+        version: '6.8'
+      ).converge(described_recipe)
+    end
+
+    it 'renders the yum repository with defaults' do
+      expect(chef_run).to create_yum_repository('erlang_solutions').with(
+        repositoryid: 'erlang_solutions',
+        baseurl: 'http://packages.erlang-solutions.com/rpm/centos/6/$basearch'
       )
     end
   end
