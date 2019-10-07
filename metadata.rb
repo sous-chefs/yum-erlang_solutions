f8a886a9376966a47b0900f3c9333997e32e27f0
Tim Smith <tsmith84@gmail.com>
2019-10-07 15:30:07 -0700

    Resolve Cookstyle 5.8 warnings
    Minor cleanups for the latest Cookstyle
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs/Configures yum-erlang_solutions'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.0'
 
 depends 'yum-epel'
@@ -14,4 +13,4 @@ end
 
 source_url 'https://github.com/chef-cookbooks/yum-erlang_solutions'
 issues_url 'https://github.com/chef-cookbooks/yum-erlang_solutions/issues'
-chef_version '>= 12.14' if respond_to?(:chef_version)
+chef_version '>= 12.14'
